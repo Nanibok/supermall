f7b376c7cab4d83d6ead7fbcb91ca7964c6582da
Nanibok <heluodesu@gmail.com>
2020-05-20 15:56:12 +0800

    底部导航

---
+++ src/views/home/Home.vue
@@ -0,0 +1,17 @@
+<template>
+      <h2>Home</h2>
+</template>
+
+<script>
+export default {
+  name: '',
+  data() { 
+    return {
+
+    }
+  }
+ }
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/profile/Profile.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="">
+      <h2>Profile</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data() { 
+    return {
+
+    }
+  }
+ }
+</script>
+
+<style scoped>
+</style>
